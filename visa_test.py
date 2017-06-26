2d6913dd932f938e9a705bed7bde0a1c1d0c3887
Fuming Qiu <fumingq@gmail.com>
2017-06-26 16:55:01 -0400

    Added .py test file bc can't do that on Macs for some reason

--- visa_test.py
+++ visa_test.py
@@ -5,7 +5,7 @@ inital pyvisa test for GPIB communcation
 """
 
 import visa
-import time
+#import time
 
 #encode string to ascii code
 def enc(str):
@@ -15,18 +15,36 @@ def enc(str):
 #initialize the resource manager
 rm = visa.ResourceManager()
 instList = rm.list_resources()
-gpibName = ''
+
+
+print(instList)
 
 #get the GPIB port, set up inst object. then set timeout to 1s
+"""
+this code is currently useless since only one resource and it's not a 
+gpib port. it's still serial. just fudging with taking the first index for now
+gpibName = ''
 for instr in instList:
     if instr.find('GPIB') == 0:
         gpibName = instr
-sr400 = rm.open_resource(gpibName)
+"""
+
+sr400 = rm.open_resource(instList[0])
+print(sr400.write('cs'))
+
+"""
 sr400.timeout = 1000
+
+print(sr400)
+print(type(sr400))
+
 
 #set count time, set num counts, set dwell time, reset counts, press start
 sr400.write_raw(enc('cp2, 1e7; np5; dt 1e-1; cr; cs \r'))
 
+"""
+
+"""
 #set a pause
 time.sleep(6)
 
@@ -43,3 +61,5 @@ data = sr400.read()
 print('using read')
 print(data)
 print(type(data))
+
+"""
