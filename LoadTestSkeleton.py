a8a03b19b708fc0e2ae510b3e72b5eeffa55e8f8
HoughtonYonge <hvyonge@gmail.com>
2017-06-26 16:45:46 -0400

    Adding Test Files

---
+++ LoadTestSkeleton.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 16:33:03 2017
+
+@author: pguest
+"""
+
+import sys
+from PyQt5.QtWidgets import *
+from ui_imagedialog import
